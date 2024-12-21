7f00d278b6aaf5c4225cca99c7fc828c2ca7b9c9
David Sabalete Rodríguez <dsabalete@gmail.com>
2024-12-21 18:00:07 +0100

    feat: implement semantic commits

---
+++ release.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer', // Analyzes commits to determine the next version
+    '@semantic-release/release-notes-generator', // Generates release notes
+    '@semantic-release/changelog', // Updates the CHANGELOG.md
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { path: 'CHANGELOG.md', label: 'Changelog' },
+          { path: 'dist/*.js', label: 'Compiled JS files' },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
