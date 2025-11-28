47c27c5bbb83d415d7d7f5f7deedbb88e2504bdb
David Sabalete Rodríguez <dsabalete@gmail.com>
2025-11-28 13:28:58 +0100

    chore: update GitHub release configuration to include only CHANGELOG.md as an asset

--- release.config.js
+++ release.config.js
@@ -5,15 +5,14 @@ module.exports = {
     '@semantic-release/release-notes-generator', // Generates release notes
     '@semantic-release/changelog', // Updates the CHANGELOG.md
     '@semantic-release/npm', // Updates package.json
-    // [
-    //   '@semantic-release/github',
-    //   {
-    //     assets: [
-    //       { path: 'CHANGELOG.md', label: 'Changelog' },
-    //       { path: 'dist/*.js', label: 'Compiled JS files' },
-    //     ],
-    //   },
-    // ],
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { path: 'CHANGELOG.md', label: 'Changelog' },
+        ],
+      },
+    ],
     [
       '@semantic-release/git',
       {
